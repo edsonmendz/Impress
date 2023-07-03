2b9dd7de8fb0d136b990bd14e41779675f8d5506
edsonmendz <japaact@gmail.com>
2023-07-03 20:23:26 -0300

    Jquery funcionando?

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
 $(document).ready(function() {
 
+    // coloração da sidebar
     $(".sidebar ul li").on('click', function (){
       $(".sidebar ul li.active").removeClass('active');            
       $(this).addClass('active');      
     });
 
+
+    // chamar sidebar
     $('.open-btn').on('click', function() {
       $('.sidebar').addClass('active');      
     });
     $('.close-btn').on('click', function() {
       $('.sidebar').removeClass('active');      
     });
+
+    // chamar páginas
+    $('#quem_sou').on('click', () => {      
+      $.get('paginas/quem_sou.html', data => {
+          $('#principal').html(data)
+      })
+  })
 })
